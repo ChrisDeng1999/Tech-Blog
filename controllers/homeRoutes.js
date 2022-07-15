450f845f753cee367d904bbeb990eb486f2d7601
sagegod <cdeng1999@gmail.com>
2022-07-15 09:36:03 -0700

    added connection js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,6 +3,7 @@ const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
 
+
 router.get('/', (req, res) => {
     // If the user is already logged in, redirect the request to another route
     if (req.session.logged_in) {

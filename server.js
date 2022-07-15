f9fa0b838184cfcccf8f132b7a04e7ccebfb02c0
sagegod <cdeng1999@gmail.com>
2022-07-15 10:01:52 -0700

    got server running

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-
-
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
+const hbs = exphbs.create();
 
 const sess = {
   secret: 'Super secret secret',
@@ -25,7 +24,7 @@ const sess = {
 app.use(session(sess));
 
 // Inform Express.js on which template engine to use
-
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());

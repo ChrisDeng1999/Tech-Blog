5cef85f24fbecbd59531338e2ad44a149fbb7b6a
sagegod <cdeng1999@gmail.com>
2022-07-18 18:20:36 -0700

    added all handlebar files

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,10 +4,10 @@ const withAuth = require('../utils/auth');
 
 
 
-router.get('/', (req, res) => {
+router.get('/login', (req, res) => {
     // If the user is already logged in, redirect the request to another route
     if (req.session.logged_in) {
-      res.redirect('/create');
+      res.redirect('/');
       return;
     }
   
